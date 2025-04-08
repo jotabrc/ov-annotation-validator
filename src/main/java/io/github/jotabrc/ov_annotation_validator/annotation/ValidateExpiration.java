add6a14a152f476a989c1f790b25c9545f029e9c
jotabrc <ojcroveda@gmail.com>
2025-04-07 21:39:23 -0300

    feat: Validate Expiration

---
+++ src/main/java/io/github/jotabrc/ov_annotation_validator/annotation/ValidateExpiration.java
@@ -0,0 +1,24 @@
+package io.github.jotabrc.ov_annotation_validator.annotation;
+
+import io.github.jotabrc.ov_annotation_validator.validation.ExpirationValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for LocalDateTime fields.
+ */
+@Constraint(validatedBy = ExpirationValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidateExpiration {
+    String message() default "Token expired";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String fieldName();
+    int expirationDays();
+}
---
+++ src/main/java/io/github/jotabrc/ov_annotation_validator/validation/ExpirationValidator.java
@@ -0,0 +1,38 @@
+package io.github.jotabrc.ov_annotation_validator.validation;
+
+import io.github.jotabrc.ov_annotation_validator.annotation.ValidateExpiration;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * GlobalValidator for common fields.
+ */
+@Component
+public class ExpirationValidator implements ConstraintValidator<ValidateExpiration, LocalDateTime> {
+
+    private int expirationDays;
+    private String errorMessage;
+
+    @Override
+    public void initialize(ValidateExpiration constraintAnnotation) {
+        expirationDays = constraintAnnotation.expirationDays();
+        errorMessage = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(@NotNull @NotBlank LocalDateTime expiration, ConstraintValidatorContext constraintValidatorContext) {
+
+        if (LocalDateTime.now().minusDays(expirationDays).isBefore(expiration)) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate(errorMessage)
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+}
